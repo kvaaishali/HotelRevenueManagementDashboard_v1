411039632b34ac2d260dc122a771752fffd0cb4e
kvaaishali <kondavaishali@klh.edu.in>
2026-05-31 17:37:26 +0530

    Add files via upload

---
+++ SignupPage.jsx
@@ -0,0 +1,15 @@
+
+function SignupPage() {
+  return (
+    <div className="form">
+      <h2>Signup</h2>
+
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+
+      <button>Register</button>
+    </div>
+  );
+}
+
+export default SignupPage;
